84971e2ef78dede3b8d99e5208a61d2225768e5e
Firenze Higa Putra <firenzehiga@gmail.com>
2025-06-29 17:14:56 +0700

    Refactor code structure for improved readability and maintainability

--- src/utils/constants/playlistData.js
+++ src/utils/constants/playlistData.js
@@ -1,19 +1,23 @@
+import lana from "../../assets/img/yes.jpg";
+import weeknd from "../../assets/img/timeless.jpg";
+import taylor from "../../assets/img/lover.jpg";
+
 const playlistData = [
 	{
 		id: 7,
-		image: "../../public/photo/yes.jpg",
+		image: lana,
 		title: "Yes To Heaven",
 		artist: "Lana Del Rey",
 	},
 	{
 		id: 8,
-		image: "../../public/photo/timeless.jpg",
+		image: weeknd,
 		title: "Timeless",
 		artist: "The Weeknd",
 	},
 	{
 		id: 9,
-		image: "../../public/photo/lover.jpg",
+		image: taylor,
 		title: "Lover",
 		artist: "Taylor Swift",
 	},
